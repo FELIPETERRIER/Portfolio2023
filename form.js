0c61286650731024a6f13048ed9ed8456683d148
Felipe Bitencourt <Felipeterrier@yahoo.com.br>
2023-06-24 00:40:14 -0300

    Atualizando formulario

---
+++ form.js
@@ -0,0 +1,67 @@
+class FormSubmit{
+	constructor(settings){
+		this.settings = settings;
+		this.form = document.querySelector(settings.form);
+		this.formButton = document.querySelector(settings.button);
+		if (this.form) {
+		this.url = this.form.getAttribute('action')
+	}
+	
+
+}
+displaySucess() {
+	this.form.innerHTML = this.settings.sucess;
+
+}
+displayError(){
+	this.form.innerHTML = this.settings.error;
+}
+getFormObject(){
+	const formObject = {};
+	const fields = this.form.querySelectorAll("[name]");
+	fields.forEach((field) => {
+		formObject[field.getAttribute("name")] =field.value;
+	});
+
+	return getFormObject;
+}
+onSubmission(event){
+	event.preventDefault();
+	event.target.disabled = true;
+	event.target.innerText = 'Enviando...';
+}
+
+async sendForm(event){
+	try{
+		this.onSubmission(event);
+		await fetch(this.url,{
+		method: 'POST',
+		headers:{
+			"Content-Type":"application/json",
+			Accept:"application/json",
+		},
+		body:JSON.stringfy(this.getFormObject()),
+	});
+	this.displaySucess();}
+	catch(error){
+		this.displayError();
+		throw new Error(error);
+	}
+}
+init(){
+	if (this.form)this.formButton.addEventListener("click",() => this.sendForm);
+	return this;
+
+	}
+}
+
+const formSubmit = new FormSubmit({
+form: '[data-form]',
+button:'[data-button]',
+sucess:"<h1 class= 'sucess'>sucesso</h1>",
+error:"<h1 class= 'error'>deu ruim</h1>",
+});
+formSubmit.init();
+
+
+
